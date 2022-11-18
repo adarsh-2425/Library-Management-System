697ff349746639b490c5094f2b6df6c082caa4f5
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-11-18 13:10:34 +0530

    dashboard

--- Frontend/src/app/components/Member/dashboard/dashboard.component.ts
+++ Frontend/src/app/components/Member/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IssuedBooksService } from 'src/app/services/issued-books.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class MemberDashboardComponent implements OnInit {
+  waitingbooks:any[] | undefined;
+  issuedbooks:any[] | undefined;
+  Name = localStorage.getItem('Name');
 
-  constructor() { }
+
+  constructor(
+    private IssuedBooksService: IssuedBooksService
+
+  ) { }
 
   ngOnInit(): void {
+
+     //Books issued
+    const waitingemail = localStorage.getItem('email');
+    this.IssuedBooksService.SubmittedBooksMember(waitingemail)
+    .subscribe((data)=>{
+      this.waitingbooks = JSON.parse(JSON.stringify(data))
+    })
+
+    //Waiting books
+    const issuedemail = localStorage.getItem('email');
+    this.IssuedBooksService.IssuedBooksMember(issuedemail)
+    .subscribe((data)=>{
+      this.issuedbooks = JSON.parse(JSON.stringify(data))
+    })
   }
 
 }
